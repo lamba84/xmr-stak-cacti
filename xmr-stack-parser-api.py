047055ef5a28db64aec27e35e4292ad9d8e6c4d2
lamba84 <lamba84@tiscali.it>
2017-12-30 22:13:17 +0100

    added thread results instead of total. sutable for xmr-stack v.2.0

--- xmr-stack-parser-api.py
+++ xmr-stack-parser-api.py
@@ -3,7 +3,9 @@
 Cacti parser for xmr-stak  by fireice-uk.
 This version parse api/json web page, so it works only with xmr-stak-cpu for now  
 @author: lamba84
-
+REV: 0.2
+return results by thread
+added try except in case of lost connection
 REV: 0.1
 intial release
 
@@ -35,10 +37,18 @@ else:
     hash_url='http://'+args.url+'/api.json'
     
 def get_rate_api(h_url):
-    page= requests.get(h_url).json()
-    #print json.dumps(page, sort_keys=True, indent=2, separators=(',', ': '))
-    t_rates=page['hashrate']['total']
-    rtn_str= "total_2.5s:%s total_1m:%s total_15m:%s" %(t_rates[0],t_rates[1],t_rates[2])
-    return rtn_str
+    try:
+        page= requests.get(h_url).json()
+        #print json.dumps(page, sort_keys=True, indent=2, separators=(',', ': '))
+        t_rates=page['hashrate']['total']
+        total=""
+        i=0
+        for t in page['hashrate']['threads']:
+            total=total+"tot%s:%s "%(i,t[0])
+            i=i+1
+        #rtn_str= "total_2.5s:%s total_1m:%s total_15m:%s" %(t_rates[0],t_rates[1],t_rates[2])
+        return total
+    except:
+        return "tot0:0.0"
 
 print get_rate_api(hash_url)
